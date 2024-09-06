7db71dde7198c799fff6d96d5e7517f419463ee7
Taump <taump@yandex.ru>
2024-09-06 16:02:47 +0400

    feat: add widgets

--- src/app/sys/[key]/page.tsx
+++ src/app/sys/[key]/page.tsx
@@ -1,7 +1,9 @@
+import { notFound } from "next/navigation";
+
+import { Widgets } from "@/components/layouts/widgets";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { transformSysVarKeyToName } from "@/lib/transformSysVarKeyToName";
 import { sysVarConfiguration } from "@/sysVarConfiguration";
-import { notFound } from "next/navigation";
 
 interface ISysVarPageProps {
 	params: {
@@ -21,35 +23,8 @@ export default function SysVarPage({ params }: ISysVarPageProps) {
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quibusdam impedit maiores recusandae accusamus dolorem praesentium a excepturi distinctio, rem qui aliquid ipsam iure iste eaque dolorum commodi officiis in!
 		</div>
 
-		<div className="grid grid-cols-4 gap-8">
-			<Card>
-				<CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-					<CardTitle className="text-sm font-medium">Total voters balance</CardTitle>
-					<small>GBYTE</small>
-				</CardHeader>
-				<CardContent>
-					<div className="text-2xl font-bold">45,231.89</div>
-					{/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
-				</CardContent>
-			</Card>
-
-			<Card>
-				<CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-					<CardTitle className="text-sm font-medium">Count votes</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<div className="text-2xl font-bold">432</div>
-				</CardContent>
-			</Card>
-
-			<Card className="max-w-xs">
-				<CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-					<CardTitle className="text-sm font-medium">Current value</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<div className="text-2xl font-bold">423423</div>
-				</CardContent>
-			</Card>
-		</div>
+		<Widgets
+			param_key={params.key}
+		/>
 	</div>)
 }
--- src/services/httpHub.ts
+++ src/services/httpHub.ts
@@ -1,6 +1,44 @@
-const CACHE_REVALIDATE_TIME = 12 * 3600; // 12 hours
+import appConfig from "@/appConfig";
+
+const CACHE_LIST_REVALIDATE_TIME = 12 * 3600; // 12 hours
+const CACHE_VARS_REVALIDATE_TIME = 1 * 3600; // 1 hour
+const CACHE_VARS_VOTES_REVALIDATE_TIME = 360; // 6 minutes
 
 export const getSystemVarsList = async (): Promise<string[]> => {
-	const data = await fetch('https://testnet.obyte.org/api/get_system_vars', { next: { revalidate: CACHE_REVALIDATE_TIME, tags: ['list'] }, method: "POST" }).then(res => res.json()).then(({ data }) => data);
+	const data = await fetch(`https://${appConfig.TESTNET ? 'testnet.' : ''}obyte.org/api/get_system_vars`, { next: { revalidate: CACHE_LIST_REVALIDATE_TIME, tags: ['list'] }, method: "POST" }).then(res => res.json()).then(({ data }) => data);
 	return Object.keys(data);
+}
+
+interface ISystemVarsList {
+	[key: string]: {
+		vote_count_mci: number;
+		value: number | string[];
+		is_emergency: number;
+	}[]
+}
+
+export const getSystemVars = async (): Promise<ISystemVarsList> => {
+	const data = await fetch(`https://${appConfig.TESTNET ? 'testnet.' : ''}obyte.org/api/get_system_vars`, { next: { revalidate: CACHE_VARS_REVALIDATE_TIME, tags: ['vars'] }, method: "POST" }).then(res => res.json()).then(({ data }) => data);
+	return data;
+}
+
+interface IVoteInfo {
+	address: string;
+	unit: string;
+	timestamp: string;
+	ops?: string[];
+	value: number;
+}
+interface IVotes {
+	votes: {
+		[key: string]: IVoteInfo[];
+	},
+	balances: {
+		[address: string]: number;
+	}
+}
+
+export const getSystemVarsVotes = async (): Promise<IVotes> => {
+	const data = await fetch(`https://${appConfig.TESTNET ? 'testnet.' : ''}obyte.org/api/get_system_var_votes`, { next: { revalidate: CACHE_VARS_VOTES_REVALIDATE_TIME, tags: ['votes'] }, method: "POST" }).then(res => res.json()).then(({ data }) => data);
+	return data;
 }
