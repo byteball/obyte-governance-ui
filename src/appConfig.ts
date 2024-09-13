08955eb34ad30669cb21a6150a3621199fdd93e3
Taump <taump@yandex.ru>
2024-09-13 20:08:17 +0400

    fix: types

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,4 +1,13 @@
-export default {
+interface IAppConfig {
+	TESTNET: boolean;
+	VOTING_TOKEN_SYMBOL: string;
+	VOTING_TOKEN_DECIMALS: number;
+	PROVIDER_DICTIONARY: {
+		[key: string]: string;
+	};
+}
+
+const appConfig: IAppConfig = {
 	TESTNET: true,
 	VOTING_TOKEN_SYMBOL: "GBYTE",
 	VOTING_TOKEN_DECIMALS: 9,
@@ -6,4 +15,6 @@ export default {
 		"F4KHJUCLJKY4JV7M5F754LAJX4EB7M4N": "Tony Churyumoff",
 		"WMFLGI2GLAB2MDF2KQAH37VNRRMK7A5N": "Aleksandr Ponomarev (testnet)"
 	}
-}
+};
+
+export default appConfig;
