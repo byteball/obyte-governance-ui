bc9374d01144a1b08cb4fbe5e583fb02d2c0212e
Taump <taump@yandex.ru>
2024-09-19 23:23:33 +0400

    feat: add sorting

--- src/app/sys/[key]/userVotes.tsx
+++ src/app/sys/[key]/userVotes.tsx
@@ -70,8 +70,10 @@ export const UserVotes: FC<IUserVotesProps> = async ({ param_key }) => {
 						<CardContent>
 							<div className="font-bold mb-2">Votes:</div>
 							<div className="flex flex-col gap-4">
-								{votes.sort((a, b) => balances[b.address] - balances[a.address]).map(({ address, unit, timestamp }) => (
-									<div key={unit + timestamp} className="flex justify-between items-center">
+								{votes.sort((a, b) => (balances[b.address] ?? 0) - (balances[a.address] ?? 0)).map(({ address, unit, timestamp }) => {
+									if (!balances[address]) return null;
+
+									return (<div key={unit + timestamp} className="flex justify-between items-center">
 										<div className="">
 											<a target="_blank" rel="noreferrer" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${address}`} className="block font-medium">
 												{address}
@@ -79,7 +81,7 @@ export const UserVotes: FC<IUserVotesProps> = async ({ param_key }) => {
 
 											<div className="flex space-x-4 align-middle text-xs text-gray-500">
 												{unit ? <a className="hover:text-gray-900" target="_blank" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/${unit}`}>Unit on explorer <ExternalLink className="inline h-3 w-3" /></a> : null}
-												{timestamp ? <div className="">{moment.unix(timestamp).format("LLL")}</div> : null}
+												{timestamp ? <div>{moment.unix(timestamp).format("LLL")}</div> : null}
 											</div>
 										</div>
 
@@ -90,8 +92,8 @@ export const UserVotes: FC<IUserVotesProps> = async ({ param_key }) => {
 												decimals={appConfig.VOTING_TOKEN_DECIMALS}
 											/> {appConfig.VOTING_TOKEN_SYMBOL}
 										</div>
-									</div>
-								))}
+									</div>)
+								})}
 							</div>
 						</CardContent>
 						<CardFooter>
