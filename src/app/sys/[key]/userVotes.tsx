8de0e7d26facbe3693fdd9e1d71fa346720b143e
Taump <taump@yandex.ru>
2024-09-19 23:48:27 +0400

    refactor: clean up user votes display code for better readability

--- src/app/sys/[key]/userVotes.tsx
+++ src/app/sys/[key]/userVotes.tsx
@@ -75,33 +75,30 @@ export const UserVotes: FC<IUserVotesProps> = async ({ param_key }) => {
 									decimals={appConfig.VOTING_TOKEN_DECIMALS}
 								/> {appConfig.VOTING_TOKEN_SYMBOL}
 							</div>
-							
+
 							<div className="font-bold mb-2">Votes:</div>
 							<div className="flex flex-col gap-4">
-								{votes.sort((a, b) => (balances[b.address] ?? 0) - (balances[a.address] ?? 0)).map(({ address, unit, timestamp }) => {
-									if (!balances[address]) return null;
+								{votes.sort((a, b) => (balances[b.address] ?? 0) - (balances[a.address] ?? 0)).map(({ address, unit, timestamp }) => (<div key={unit + timestamp} className="flex justify-between items-center">
+									<div className="">
+										<a target="_blank" rel="noreferrer" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${address}`} className="block font-medium">
+											{address}
+										</a>
 
-									return (<div key={unit + timestamp} className="flex justify-between items-center">
-										<div className="">
-											<a target="_blank" rel="noreferrer" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/address/${address}`} className="block font-medium">
-												{address}
-											</a>
-
-											<div className="flex space-x-4 align-middle text-xs text-gray-500">
-												{unit ? <a className="hover:text-gray-900" target="_blank" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/${unit}`}>Unit on explorer <ExternalLink className="inline h-3 w-3" /></a> : null}
-												{timestamp ? <div>{moment.unix(timestamp).format("LLL")}</div> : null}
-											</div>
+										<div className="flex space-x-4 align-middle text-xs text-gray-500">
+											{unit ? <a className="hover:text-gray-900" target="_blank" href={`https://${appConfig.TESTNET ? 'testnet' : ''}explorer.obyte.org/${unit}`}>Unit on explorer <ExternalLink className="inline h-3 w-3" /></a> : null}
+											{timestamp ? <div>{moment.unix(timestamp).format("LLL")}</div> : null}
 										</div>
+									</div>
 
-										<div className="font-bold">
-											<ParamsView
-												type="number"
-												value={balances[address] || 0}
-												decimals={appConfig.VOTING_TOKEN_DECIMALS}
-											/> {appConfig.VOTING_TOKEN_SYMBOL}
-										</div>
-									</div>)
-								})}
+									<div className="font-bold">
+										<ParamsView
+											type="number"
+											value={balances[address] || 0}
+											decimals={appConfig.VOTING_TOKEN_DECIMALS}
+										/> {appConfig.VOTING_TOKEN_SYMBOL}
+									</div>
+								</div>)
+								)}
 							</div>
 						</CardContent>
 					</Card>)
