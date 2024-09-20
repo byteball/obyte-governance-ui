c914834073aaa9b3f643b840896d56b2468299f0
Taump <taump@yandex.ru>
2024-09-21 02:49:52 +0400

    feat: update Home component layout with new headings and descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,16 @@ import { SysVarItemList } from "@/components/layouts/sys-var-item-list";
 
 export default function Home() {
 	return (<div className="space-y-16">
-		<div className="space-y-8">
+		<div className="space-y-8 mt-14">
+			<h1 className="text-5xl font-semibold tracking-tight scroll-m-20 lg:text-6xl">
+				Obyte network governance
+			</h1>
+
+			<p className="max-w-3xl text-xl text-muted-foreground">
+				A modal dialog that interrupts the user with important content and expects
+				a response important content and expects a response
+			</p>
+
 			<div className="hidden mb-8 sm:mt-32 sm:flex lg:mt-16">
 				<div className="relative px-3 py-1 text-sm leading-6 text-gray-500 rounded-full ring-1 ring-gray-900/10 dark:ring-white/10 hover:ring-gray-900/20">
 					We wrote an article about it.{' '}
@@ -12,18 +21,13 @@ export default function Home() {
 					</a>
 				</div>
 			</div>
-
-			<h1 className="text-4xl font-semibold tracking-tight scroll-m-20 lg:text-6xl">
-				Obyte network <br /> governance
-			</h1>
-
-			<p className="max-w-xl text-xl text-muted-foreground">
-				A modal dialog that interrupts the user with important content and expects
-				a response.
-			</p>
 		</div>
 
 		<div>
+			<h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-4">
+				List of parameters
+			</h2>
+
 			<SysVarItemList />
 		</div>
 	</div>
