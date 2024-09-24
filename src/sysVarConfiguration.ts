ca4265340589c7662bfbce1c597f0ea5420a5314
Taump <taump@yandex.ru>
2024-09-24 22:50:51 +0400

    feat: add desc

--- src/sysVarConfiguration.ts
+++ src/sysVarConfiguration.ts
@@ -8,28 +8,28 @@ interface ISysVarConfiguration {
 
 export const sysVarConfiguration: ISysVarConfiguration = {
 	op_list: { // list of order providers type: string[]
-		customName: "Order providers",
-		description: "33An extension of Obyte consensus protocol is proposed which allows users to continuously vote for their preferred list of Order Providers (OPs).",
+		customName: "Order Providers",
+		description: "List of 12 addresses whose transactions are used to order all other transactions in the DAG.",
 		type: "op-list"
 	},
 	threshold_size: { // OIP-05 We introduce an additional fee, called "oversize fee", paid by transactions whose size exceeds threshold_size. The threshold size is initially set to 10 kB but can be changed by governance (see below). type: number
 		customName: "Threshold size",
-		description: "The size of the threshold unit.",
+		description: "Transactions whose size exceeds this threshold are charged an oversize fee.",
 		type: "number"
 	},
 	base_tps_fee: { // OIP-06 - type: number (default 10)
 		customName: "Base TPS fee",
-		description: "The base fee per transaction.",
+		description: "A parameter used in calculation of TPS fee. The fee linearly depends on this parameter.",
 		type: "number"
 	},
 	tps_interval: { // OIP-06 - type: number (default 1)
 		customName: "TPS interval",
-		description: "The interval between two consecutive transactions.",
+		description: "A parameter that determines how steep the growth of the fee is as TPS grows.",
 		type: "number"
 	},
 	tps_fee_multiplier: { // OIP-06 - type: number (default 10)
 		customName: "TPS fee multiplier",
-		description: "The fee multiplier per transaction.",
+		description: "A multiplier that determines how many times larger fee should be prepaid compared with the estimated TPS fee.",
 		type: "number"
 	},
 } as const;
