a12f339c63b35f22bdf69f316e850d96afc6b263
Taump <taump@yandex.ru>
2024-09-27 15:25:47 +0400

    fix: add antialiased class to improve text rendering in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 					disableTransitionOnChange
 					storageKey="obyte-governance-theme"
 				>
-					<div className="flex flex-col w-full min-h-screen">
+					<div className="flex flex-col w-full min-h-screen antialiased">
 						<Header />
 
 						<main className="flex container max-w-6xl min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
