1edcbb5023d47ad439f16a3add2b6551b3cb6e4e
Taump <taump@yandex.ru>
2024-09-27 15:12:38 +0400

    fix: improve header layout and accessibility features

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -33,64 +33,65 @@ export const Header = () => {
 				}}
 			/>
 
-		<header className="sticky top-0 z-50 flex items-center h-16 gap-4 px-4 border-b bg-background md:px-6">
-			<nav className="flex-col w-full hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-				<Link
-					href="/"
-					className="flex items-center h-8 text-lg font-medium md:text-base flex-shrink-0"
-				>
-					<img src="/logo.svg" alt="Obyte governance logo" className="inline-block h-8" />
-				</Link>
-				{Object.entries(sysVarConfiguration).map(([key, { customName }]) => (<Link
-					href={`/sys/${key}`}
-					key={key}
-					className={cn("hover:text-foreground block flex-shrink-0", { "text-foreground text-gray-950 dark:text-gray-300": pathname === `/sys/${key}` }, { "text-muted-foreground": pathname !== `/sys/${key}` })}
-				>
-					{customName || transformSysVarKeyToName(key)}
-				</Link>))}
-			</nav>
-			<Sheet>
-				<VisuallyHidden.Root>
-					<SheetTitle />
-				</VisuallyHidden.Root>
-				<SheetTrigger asChild>
-					<Button
-						variant="outline"
-						ref={ref}
-						size="icon"
-						className="shrink-0 md:hidden"
+			<header className="sticky top-0 z-50 flex items-center h-16 gap-4 px-4 border-b bg-background md:px-6">
+				<nav className="flex-col w-full hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+					<Link
+						href="/"
+						className="flex text-lg font-medium md:text-base flex-shrink-0"
 					>
-						<Menu className="w-5 h-5" />
-						<span className="sr-only">Toggle navigation menu</span>
-					</Button>
-				</SheetTrigger>
-				<SheetContent side="left">
-					<nav className="grid gap-6 text-lg font-medium">
-						<Link
-							href="/"
-							onClick={() => ref.current?.click()}
-							className="flex items-center gap-2 text-lg font-semibold"
+						<img src="/logo.svg" alt="Obyte governance logo" className="inline-block h-8" />
+						<div className="align-middle text-xs -ml-3"><span className="text-[#456C91]">Governance</span></div>
+					</Link>
+					{Object.entries(sysVarConfiguration).map(([key, { customName }]) => (<Link
+						href={`/sys/${key}`}
+						key={key}
+						className={cn("hover:text-foreground block flex-shrink-0", { "text-foreground text-gray-950 dark:text-gray-300": pathname === `/sys/${key}` }, { "text-muted-foreground": pathname !== `/sys/${key}` })}
+					>
+						{customName || transformSysVarKeyToName(key)}
+					</Link>))}
+				</nav>
+				<Sheet>
+					<VisuallyHidden.Root>
+						<SheetTitle />
+					</VisuallyHidden.Root>
+					<SheetTrigger asChild>
+						<Button
+							variant="outline"
+							ref={ref}
+							size="icon"
+							className="shrink-0 md:hidden"
 						>
-							<img src="/logo.svg" alt="Obyte governance logo" className="h-8" />
-							<span className="sr-only">Obyte governance</span>
-						</Link>
-						{Object.entries(sysVarConfiguration).map(([key, { customName }]) => (<Link
-							href={`/sys/${key}`}
-							key={key}
-							onClick={() => ref.current?.click()}
-							className={cn("text-muted-foreground hover:text-foreground", { "font-bold text-foreground text-gray-950 dark:text-gray-300": pathname === `/sys/${key}` })}
-						>
-							{customName || key}
-						</Link>))}
-					</nav>
-				</SheetContent>
-			</Sheet>
-			<div className="flex flex-1">
-				<div className="flex justify-end w-full">
-					<ModeToggle />
+							<Menu className="w-5 h-5" />
+							<span className="sr-only">Toggle navigation menu</span>
+						</Button>
+					</SheetTrigger>
+					<SheetContent side="left">
+						<nav className="grid gap-6 text-lg font-medium">
+							<Link
+								href="/"
+								onClick={() => ref.current?.click()}
+								className="flex items-center gap-2 text-lg font-semibold"
+							>
+								<img src="/logo.svg" alt="Obyte governance logo" className="h-8" />
+								<span className="sr-only">Obyte governance</span>
+							</Link>
+							{Object.entries(sysVarConfiguration).map(([key, { customName }]) => (<Link
+								href={`/sys/${key}`}
+								key={key}
+								onClick={() => ref.current?.click()}
+								className={cn("text-muted-foreground hover:text-foreground", { "font-bold text-foreground text-gray-950 dark:text-gray-300": pathname === `/sys/${key}` })}
+							>
+								{customName || key}
+							</Link>))}
+						</nav>
+					</SheetContent>
+				</Sheet>
+				<div className="flex flex-1">
+					<div className="flex justify-end w-full">
+						<ModeToggle />
+					</div>
 				</div>
-			</div>
-		</header>
+			</header>
 		</>
 	)
 }
