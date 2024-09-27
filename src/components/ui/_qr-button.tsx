add18f32218f0f024ed560cf5c92db9f1cb8dad8
Taump <taump@yandex.ru>
2024-09-27 15:08:21 +0400

    feat: enhance QRButton component with keyboard accessibility and improved tooltip content

--- src/components/ui/_qr-button.tsx
+++ src/components/ui/_qr-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { forwardRef } from "react";
+import { forwardRef, useRef } from "react";
 import { QrCode as QrCodeIcon } from "lucide-react";
 import { QRCodeSVG } from "qrcode.react";
 import cn from "classnames";
@@ -14,50 +14,60 @@ interface IQRButtonProps extends ButtonProps {
 	fluid?: boolean;
 }
 
-export const QRButton = forwardRef<HTMLAnchorElement, IQRButtonProps>(({ className, fluid, children, href, disabled = false, ...props }, ref) => (<div className="flex">
-	<Dialog>
+export const QRButton = forwardRef<HTMLAnchorElement, IQRButtonProps>(({ className, fluid, children, href, disabled = false, ...props }, ref) => {
+	const insideLinkRef = useRef<HTMLButtonElement>(null);
+
+	const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+		if (event.key === 'Enter') {
+			insideLinkRef && insideLinkRef?.current?.click();
+		}
+	};
+
+	return (<div onKeyDown={handleKeyDown} tabIndex={0} className="flex rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0">
+		<Dialog>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<DialogTrigger asChild onClick={(e) => e.stopPropagation()}>
+							<Button tabIndex={-1} {...props} disabled={disabled} className="px-3 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-tr-none rounded-br-none">
+								<QrCodeIcon className="w-4 h-4" />
+							</Button>
+						</DialogTrigger>
+					</TooltipTrigger>
+					<TooltipContent className="max-w-[250px]">
+						<p>Send the transaction from your mobile phone</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle className="max-w-[200px] text-center mx-auto">Scan this QR code with your mobile phone</DialogTitle>
+				</DialogHeader>
+				<div className="mx-auto">
+					<a href={href}>
+						<QRCodeSVG size={240} className="qr" bgColor='#24292e' fgColor='#fff' marginSize={1} value={href} />
+					</a>
+				</div>
+
+				<div className='text-xs text-foreground max-w-[220px] mx-auto text-center'>
+					Install Obyte wallet for <a className='text-blue-700 dark:text-blue-400' href="https://itunes.apple.com/us/app/byteball/id1147137332?ls=1&mt=8" target="_blank" rel="noreferrer">iOS</a> or <a className='text-blue-700 dark:text-blue-400' href="https://play.google.com/store/apps/details?id=org.byteball.wallet" target="_blank" rel="noreferrer">Android</a> if you don't have one yet
+				</div>
+			</DialogContent>
+		</Dialog>
 		<TooltipProvider>
 			<Tooltip>
 				<TooltipTrigger asChild>
-					<DialogTrigger asChild onClick={(e) => e.stopPropagation()}>
-						<Button {...props} disabled={disabled} className="px-3 rounded-tr-none rounded-br-none">
-							<QrCodeIcon className="w-4 h-4" />
-						</Button>
-					</DialogTrigger>
+					<Button {...props} ref={insideLinkRef} tabIndex={-1} disabled={disabled} asChild className={cn("pl-2 rounded-tl-none rounded-bl-none cursor-pointer focus-visible:ring-0 focus-visible:ring-offset-0", { "pointer-events-none opacity-50 select-none": disabled }, { "w-full": fluid })}>
+						<a href={href} ref={ref}>
+							{children}
+						</a>
+					</Button>
 				</TooltipTrigger>
 				<TooltipContent className="max-w-[250px]">
-					<p>Send the transaction from your mobile phone</p>
+					<p>This will open your Obyte wallet installed on this computer and send the transaction</p>
 				</TooltipContent>
 			</Tooltip>
 		</TooltipProvider>
-
-		<DialogContent className="sm:max-w-[425px]">
-			<DialogHeader>
-				<DialogTitle className="max-w-[200px] text-center mx-auto">Scan this QR code with your mobile phone</DialogTitle>
-			</DialogHeader>
-			<div className="mx-auto">
-				<a href={href}>
-					<QRCodeSVG size={240} className="qr" bgColor='#24292e' fgColor='#fff' value={href} />
-				</a>
-			</div>
-
-			<div className='text-xs text-foreground max-w-[220px] mx-auto text-center'>
-				Install Obyte wallet for <a className='text-blue-700 dark:text-blue-400' href="https://itunes.apple.com/us/app/byteball/id1147137332?ls=1&mt=8" target="_blank" rel="noreferrer">iOS</a> or <a className='text-blue-700 dark:text-blue-400' href="https://play.google.com/store/apps/details?id=org.byteball.wallet" target="_blank" rel="noreferrer">Android</a> if you don't have one yet
-			</div>
-		</DialogContent>
-	</Dialog>
-	<TooltipProvider>
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Button {...props} disabled={disabled} asChild className={cn("pl-2 rounded-tl-none rounded-bl-none cursor-pointer", { "pointer-events-none opacity-50 select-none": disabled }, { "w-full": fluid })}>
-					<a href={href} ref={ref}>
-						{children}
-					</a>
-				</Button>
-			</TooltipTrigger>
-			<TooltipContent className="max-w-[250px]">
-				<p>This will open your Obyte wallet installed on this computer and send the transaction</p>
-			</TooltipContent>
-		</Tooltip>
-	</TooltipProvider>
-</div>));
+	</div>)
+});
