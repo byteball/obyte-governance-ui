4ae3a18d47884576b782fddb61ab3142a6dfe665
Taump <taump@yandex.ru>
2024-10-16 12:35:38 +0400

    fix: rename

--- src/lib/generateTpsFeeDataByParams.ts
+++ src/lib/generateTpsFeeDataByParams.ts
@@ -1,4 +1,4 @@
-const final_tps = [0.03, 0.1, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12];
+const final_tps = [0.03, 0.1, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12] as const;
 
 interface ITpsFeeParams {
 	base_tps_fee?: number;
@@ -11,13 +11,13 @@ interface IGenerateTpsFeeDataByParams {
 	newParams?: ITpsFeeParams;
 }
 
-interface TpsFeeDataResult {
+interface ITpsFeeDataResult {
 	fee: number;
 	newFee?: number;
 	tps: number;
 }
 
-export const generateTpsFeeDataByParams = ({ currentParams, newParams }: IGenerateTpsFeeDataByParams = { currentParams: {}, newParams: {} }): TpsFeeDataResult[] => {
+export const generateTpsFeeDataByParams = ({ currentParams, newParams }: IGenerateTpsFeeDataByParams = { currentParams: {}, newParams: {} }): ITpsFeeDataResult[] => {
 	const { base_tps_fee: current_base_tps_fee = 10, tps_interval: current_tps_interval = 1, tps_fee_multiplier: current_tps_fee_multiplier = 1 } = currentParams ?? {};
 	const { base_tps_fee: new_base_tps_fee = 10, tps_interval: new_tps_interval = 1, tps_fee_multiplier: new_tps_fee_multiplier = 1 } = newParams ?? {};
 
@@ -25,5 +25,5 @@ export const generateTpsFeeDataByParams = ({ currentParams, newParams }: IGenera
 		tps,
 		fee: Math.ceil(current_tps_fee_multiplier * current_base_tps_fee * (Math.exp(tps / current_tps_interval) - 1)),
 		newFee: Math.ceil(new_tps_interval ? new_tps_fee_multiplier * new_base_tps_fee * (Math.exp(tps / new_tps_interval) - 1) : 0)
-	})) satisfies TpsFeeDataResult[];
+	})) satisfies ITpsFeeDataResult[];
 }
