dec6add0b32354b328b787c00de43e9d5dad70e1
Taump <taump@yandex.ru>
2024-10-16 12:32:48 +0400

    fix: showing infinity on the chart

--- src/lib/generateOversizeFeeDataByParams.ts
+++ src/lib/generateOversizeFeeDataByParams.ts
@@ -17,14 +17,14 @@ export const generateOversizeFeeDataByParams = ({ currentParams, newParams }: IG
 	const { threshold_size: current_threshold_size = 10000 } = currentParams ?? {};
 	const { threshold_size: new_threshold_size = 10000 } = newParams ?? {};
 
-	const maxCurrentThresholdSize = Math.max(current_threshold_size, new_threshold_size)
+	const maxCurrentThresholdSize = Math.max(current_threshold_size, new_threshold_size);
 
 	const sizes = Array(5).fill(1).map((_, i) => maxCurrentThresholdSize * (i + 1));
 
 	return [0, ...sizes]
 	.map((size: number) => ({
 		size,
-		oversizeFee: Math.ceil(current_threshold_size > 1 && size !== 0 ? size * (Math.exp(size / current_threshold_size - 1) - 1) : 0),
-		newOversizeFee: Math.ceil(new_threshold_size > 1 && size !== 0 ? size * (Math.exp(size / new_threshold_size - 1) - 1) : 0),
+		oversizeFee: Math.ceil(size !== 0 ? size * (Math.exp(size / current_threshold_size - 1) - 1) : 0),
+		newOversizeFee: Math.ceil(size !== 0 ? size * (Math.exp(size / new_threshold_size - 1) - 1) : 0),
 	})) satisfies TpsFeeDataResult[];
 }
