a5a96a15e5a61d3b2b8ee69418c449cf90c789a4
tony <tonyofbyteball@users.noreply.github.com>
2024-11-09 02:35:43 +0700

    long descriptions of parameters

--- src/sysVarConfiguration.tsx
+++ src/sysVarConfiguration.tsx
@@ -12,28 +12,31 @@ interface ISysVarConfiguration {
 export const sysVarConfiguration: ISysVarConfiguration = {
 	op_list: { // list of order providers type: string[]
 		customName: "Order Providers",
-		description: <div><small>Description</small>, <b>Big</b> description</div>,
-		short_description: "List of 12 addresses whose transactions are used to order all other transactions in the DAG.",
+		description: <div>List of 12 addresses whose transactions are used to order all other transactions on the DAG. <a href="https://obyte.org/technology/order-providers" target="_blank" rel="noopener">Order providers</a> frequently post waypoint transactions that determine the order of other transactions that are not already ordered on the DAG. Order providers do not have the power to approve or confirm transactions, and are unable to block them. However, they are generally expected to be non-anonymous people or organizations interested in Obyte's well-being.</div>,
+		short_description: "List of 12 addresses whose transactions are used to order all other transactions on the DAG.",
 		type: "op-list"
 	},
 	threshold_size: { // OIP-05 We introduce an additional fee, called "oversize fee", paid by transactions whose size exceeds threshold_size. The threshold size is initially set to 10 kB but can be changed by governance (see below). type: number
 		customName: "Threshold size",
-		description: <div><small>Description</small>, <b>Big</b> description</div>,
+		description: <div>Threshold size is a parameter that affects protection against spamming the network with excessively large transactions. Transactions whose size exceeds this threshold are charged an oversize fee that rises exponentially with the growing size. See <a href="https://github.com/byteball/OIPs/blob/master/oip-0005.md" target="_blank" rel="noopener">Obyte improvement proposal 5</a> for details.</div>,
 		short_description: "Transactions whose size exceeds this threshold are charged an oversize fee.",
 		type: "number"
 	},
 	base_tps_fee: { // OIP-06 - type: number (default 10)
 		customName: "Base TPS fee",
+		description: <div>Base TPS fee is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. The fee linearly depends on Base TPS fee.</div>,
 		short_description: "A parameter used in calculation of TPS fee. The fee linearly depends on this parameter.",
 		type: "number"
 	},
 	tps_interval: { // OIP-06 - type: number (default 1)
 		customName: "TPS interval",
+		description: <div>TPS interval is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. TPS interval determines the steepness of the growth of the fee as TPS grows.</div>,
 		short_description: "A parameter that determines how steep the growth of the fee is as TPS grows.",
 		type: "number"
 	},
 	tps_fee_multiplier: { // OIP-06 - type: number (default 10)
 		customName: "TPS fee multiplier",
+		description: <div>TPS fee multiplier is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. Since the TPS, and therefore the TPS fee, becomes known only after the transaction stabilizes, users prepay a multiple of the estimated TPS fee, and TPS fee multiplier determines this multiple. If any fees are overpaid, they are used to pay for TPS fees of the future transactions.</div>,
 		short_description: "A multiplier that determines how many times larger fee should be prepaid compared with the estimated TPS fee.",
 		type: "number"
 	},
