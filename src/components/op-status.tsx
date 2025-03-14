8f2e71030f5ef021e6d47d231f9cdb10ce8e33a2
Taump <taump@yandex.ru>
2025-03-14 15:28:02 +0100

    fix: remove lastSeenMciTimestamp from OrderProviderStatus component

--- src/components/op-status.tsx
+++ src/components/op-status.tsx
@@ -17,11 +17,9 @@ interface IOrderProviderStatusProps extends IOrderProviderStats {
 	isActive: boolean;
 }
 
-export const OrderProviderStatus: FC<IOrderProviderStatusProps> = ({ validations_count: validationsCount = "0", isActive = false, status = "Unknown", total_witnessing_fees: totalWitnessingFees = "", last_seen_mci_timestamp: lastSeenMciTimestamp = "" }) => {
+export const OrderProviderStatus: FC<IOrderProviderStatusProps> = ({ validations_count: validationsCount = "0", isActive = false, total_witnessing_fees: totalWitnessingFees = "" }) => {
 
 	if (!isActive) return <StatusDot status={"unknown"} />
-
-	const lastSeenMci = moment(lastSeenMciTimestamp).isValid() ? moment.utc(lastSeenMciTimestamp).fromNow() : "> 12h";
 
 	return <TooltipProvider>
 		<Tooltip>
@@ -30,9 +28,7 @@ export const OrderProviderStatus: FC<IOrderProviderStatusProps> = ({ validations
 			</TooltipTrigger>
 			<TooltipContent>
 				<div className="text-xs">
-					<div><b>Transactions:</b> {validationsCount}</div>
-					<div><b>Last seen:</b> {lastSeenMci}</div>
-					<div><b>Origin:</b> {status}</div>
+					<div><b>Transactions in 12 hours:</b> {validationsCount}</div>
 					<div><b>Income (in bytes):</b> {totalWitnessingFees}</div>
 				</div>
 			</TooltipContent>
