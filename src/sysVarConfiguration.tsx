0b12961c9f5c94a116001392e1f158004595402a
tony <tonyofbyteball@users.noreply.github.com>
2025-03-17 18:12:30 +0700

    clearer descriptions of tps fee parameters

--- src/sysVarConfiguration.tsx
+++ src/sysVarConfiguration.tsx
@@ -24,19 +24,19 @@ export const sysVarConfiguration: ISysVarConfiguration = {
 	},
 	base_tps_fee: { // OIP-06 - type: number (default 10)
 		customName: "Base TPS fee",
-		description: <div>Base TPS fee is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. The fee linearly depends on Base TPS fee.</div>,
+		description: <div>Base TPS fee is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay an exponentially growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. The fee linearly depends on this parameter, which is measured in Bytes.</div>,
 		short_description: "A parameter used in calculation of TPS fee. The fee linearly depends on this parameter.",
 		type: "number"
 	},
 	tps_interval: { // OIP-06 - type: number (default 1)
 		customName: "TPS interval",
-		description: <div>TPS interval is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. TPS interval determines the steepness of the growth of the fee as TPS grows.</div>,
+		description: <div>TPS interval is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay an exponentially growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. Every time the TPS grows by TPS interval, the TPS fee grows <i>e</i> (≈2.71828) times.</div>,
 		short_description: "A parameter that determines how steep the growth of the fee is as TPS grows.",
 		type: "number"
 	},
 	tps_fee_multiplier: { // OIP-06 - type: number (default 10)
 		customName: "TPS fee multiplier",
-		description: <div>TPS fee multiplier is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay the growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. Since the TPS, and therefore the TPS fee, becomes known only after the transaction stabilizes, users prepay a multiple of the estimated TPS fee, and TPS fee multiplier determines this multiple. If any fees are overpaid, they are used to pay for TPS fees of the future transactions.</div>,
+		description: <div>TPS fee multiplier is a parameter that affects protection against spamming the network with too many transactions. As the load on the network (measured in transactions per second — TPS) grows, users have to pay an exponentially growing TPS fee as described in <a href="https://github.com/byteball/OIPs/blob/master/oip-0006.md" target="_blank" rel="noopener">Obyte improvement proposal 6</a>. Since the TPS, and therefore the TPS fee, becomes known only after the transaction stabilizes, users prepay a multiple of the estimated TPS fee, and TPS fee multiplier determines this multiple. If any fees are overpaid, they are used to pay for TPS fees of the future transactions.</div>,
 		short_description: "A multiplier that determines how many times larger fee should be prepaid compared with the estimated TPS fee.",
 		type: "number"
 	},
